55d8a2add146614fc8e64a05fe34090427d2385f
jazontan <42987310+jazontan@users.noreply.github.com>
2019-01-09 18:03:57 +0800

    Update and rename Lecture 1 - Simple string, string concatenation, string formatting.py to Lecture 1 - Strings.py

--- Lecture 1 - Strings.py
+++ Lecture 1 - Strings.py
@@ -1,4 +1,4 @@
-#Simple Python Strings
+#SIMPLE PYTHON STRINGS
 print ("Hello World!")
 print ("John replied "Hello World!"") #error is intentional for this line
 print ("Then Jane yelled 'Sorry John, I only got an error'") #single quote used inside double quotes  
@@ -8,13 +8,11 @@ print ("""Jane starts singing 'What if we rewrite the stars?
 print ("John sings too 'So why don’t we rewrite the stars?\nMaybe the world could be ours' ") #escape sequence for \n to move to a new line
 
 
-#String Concatenation
+#STRING CONCATENATION
 print ("Welcome to" + "CSC1620") #"string1" + "string2" = "string1string2". Preferred concatenation method for experienced programmers
 print ("Python Programming", "with lab") #"string1", "string2" = "string1 string2". Note the spacing between the 2 strings.
 
 
-#String Formatting
+#STRING FORMATTING
 print ("So that's all %s need to know about %s %s" % ("you", "Python", "string")) # %s acts as a placeholder for strings inside %( ... ) 
 print ("So that's all %-5s need to know about %10s %s" % ("you", "Python", "string")) #-5s has trailing spaces, #10s has leading spaces 
-
-
